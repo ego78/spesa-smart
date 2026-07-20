d3dff0b39e56ea8c069bbe42a0c29c20e575fe4f
ego78 <ego.giovanni@gmail.com>
2026-07-20 09:59:17 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.SPESA_SMART_CONFIG = {
-  // Incolla qui l'URL che termina con /exec ottenuto dalla distribuzione Apps Script.
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxth7YMa9-9uOPQQfmNHg6eq9wDmz2nVi31dTrktiy6AMwOWHbrv11AIHKjYvGemNyt/exec",
-  DEFAULT_LOCATION: "Sava 74028",
-  OFFERS_FILE: "data/offerte.json"
-};
